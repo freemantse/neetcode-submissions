df8918712de31ba914e592f079bc9440d7efd1ac
Freeman <szeboktse@gmail.com>
2026-05-03 00:14:53 +0800

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = {}
+
+        for i in range(len(strs)):
+            arr = [0] * 26
+            for x in strs[i]:
+                arr[ord(x) - ord('a')] += 1
+            key = tuple(arr)
+            if hash_map.get(key):
+                hash_map[key].append(strs[i])
+            else:
+                hash_map[key] = [strs[i]]
+        
+        return list(hash_map.values())
