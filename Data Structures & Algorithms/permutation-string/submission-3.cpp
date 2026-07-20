1b1e031b731590a901a8cfb24e0c29a9ee4f06a5
Freeman <szeboktse@gmail.com>
2026-07-20 23:59:08 +0800

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
+
+        vector<int> s1_freq(26, 0);
+        vector<int> s2_freq(26,0);
+        for (int i = 0; i < s1.size(); i++) {
+            s1_freq[s1[i] - 'a']++;
+            s2_freq[s2[i] - 'a']++;
+        }
+        if (s2_freq == s1_freq) return true;
+
+        for (int i = 0; i < (s2.size()-s1.size()); i++) {
+            s2_freq[s2[i] - 'a']--;
+            s2_freq[s2[i + s1.size()] - 'a']++;
+
+            if (s2_freq == s1_freq) return true;
+        }
+        return false;
+        
+
+    }
+};
