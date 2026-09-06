294ccaf04189d77763a16486e8916a71b0f41664
Freeman <szeboktse@gmail.com>
2026-09-06 19:23:29 +0800

    Add: binary-tree-postorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-3.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> result;
+    void dfs(TreeNode* node) {
+        if (!node) return;
+
+        dfs(node->left);
+        dfs(node->right);
+        result.push_back(node->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (!root) return {};
+        dfs(root);
+        return result;
+    }
+};
