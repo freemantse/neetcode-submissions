f925b6b846e9b1ed1908b4b3634210f6e3b7ee82
Freeman <szeboktse@gmail.com>
2026-09-24 21:04:47 +0800

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> result = {};
+
+    void backtrack(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[i], nums[idx]);
+            backtrack(nums, idx + 1);
+            swap(nums[i], nums[idx]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        backtrack(nums, 0);
+        return result;
+    }
+};
